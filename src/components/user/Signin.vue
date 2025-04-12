2056150518b7fc5f8af00ccca154a795e39e40ff
Fabou78 <fabou@fabou.com>
2025-04-12 13:53:51 +0100

    Sign in template completed

--- src/components/user/Signin.vue
+++ src/components/user/Signin.vue
@@ -1,1 +1,58 @@
-<template>Sign In</template>
+<script setup>
+  import { Field, Form } from 'vee-validate'
+  import * as yup from 'yup'
+  import { ref } from 'vue'
+
+  const isResgister = ref(false)
+  const formSchema = yup.object({
+    email: yup.string().required('The email is required').email('Not a valid email'),
+    password: yup.string().required('The password is required'),
+  })
+
+  function onSubmit(values, { resetForm }) {
+    console.log(values)
+  }
+</script>
+
+<template>
+  <div class="signin_container">
+    <!-- loader -->
+    <Form @submit="onSubmit" :validation-schema="formSchema">
+      <h1 v-text="!isResgister ? 'Sign in' : 'Register'"></h1>
+      <div class="form-group">
+        <Field name="email" :value="'fabrice@gmail.com'" v-slot="{ field, errors, errorMessage }">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your email"
+            v-bind="field"
+            :class="{ 'is-invalid': errors.length !== 0 }"
+          />
+          <div class="input_alert" v-if="errors.length !== 0">{{ errorMessage }}</div>
+        </Field>
+      </div>
+      <div class="form-group">
+        <Field name="password" :value="'testing123'" v-slot="{ field, errors, errorMessage }">
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Enter your password"
+            v-bind="field"
+            :class="{ 'is-invalid': errors.length !== 0 }"
+          />
+          <div class="input_alert" v-if="errors.length !== 0">{{ errorMessage }}</div>
+        </Field>
+      </div>
+      <button
+        type="submit"
+        class="btn mb-3 btn-block"
+        v-text="!isResgister ? 'Sign in' : 'Register'"
+      ></button>
+      <hr />
+      <div class="form_swap" @click="isResgister = !isResgister">
+        <span v-if="isResgister">I want to Sign in</span>
+        <span v-else>I want to register</span>
+      </div>
+    </Form>
+  </div>
+</template>
