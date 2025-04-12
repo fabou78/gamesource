1ccebbd2c4627303f25d0cd61a7b2c1bfa388a59
Fabou78 <fabou@fabou.com>
2025-04-12 10:19:24 +0100

    Configured firebase (not tracked by git) and home router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,3 @@
 <template>
-  Home
+  <RouterView />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/home/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,15 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue'),
+      component: Home,
     },
   ],
 })
