58ba738d99245504e3a50227fa3d4fea979563bd
Fabou78 <fabou@fabou.com>
2025-04-13 13:38:29 +0100

    Add article form

--- src/components/user/dashboard/admin/AddArticle.vue
+++ src/components/user/dashboard/admin/AddArticle.vue
@@ -1,1 +1,89 @@
-<template>Add article</template>
+<script setup>
+  import { ref } from 'vue'
+  import { Field, Form } from 'vee-validate'
+
+  const ratingArray = [0, 1, 2, 3, 4, 5]
+
+  const onSubmit = (values) => {
+    console.log(values)
+  }
+</script>
+
+<template>
+  <h1>Add Article</h1>
+  <hr />
+  <Form class="mb-5" @submit="onSubmit">
+    <div class="mb-4">
+      <Field name="game" v-slot="{ field, errors, errorMessage }">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Name of the game"
+          v-bind="field"
+          :class="{ 'is-invalid': errors.length !== 0 }"
+        />
+        <div class="input_alert" v-if="errors.length !== 0">{{ errorMessage }}</div>
+      </Field>
+    </div>
+
+    <div class="mb-4">
+      <Field name="title" v-slot="{ field, errors, errorMessage }">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="title of the article"
+          v-bind="field"
+          :class="{ 'is-invalid': errors.length !== 0 }"
+        />
+        <div class="input_alert" v-if="errors.length !== 0">{{ errorMessage }}</div>
+      </Field>
+    </div>
+
+    <div class="mb-4">
+      <Field name="excerpt" v-slot="{ field, errors, errorMessage }">
+        <textarea
+          rows="3"
+          class="form-control"
+          placeholder="Excerpt"
+          v-bind="field"
+          :class="{ 'is-invalid': errors.length !== 0 }"
+        ></textarea>
+        <div class="input_alert" v-if="errors.length !== 0">{{ errorMessage }}</div>
+      </Field>
+    </div>
+    <!-- WYSIWYG -->
+    <!-- 5-Raiting -->
+    <div class="mb-4">
+      <Field name="rating" value="Select a rating" v-slot="{ field, errors, errorMessage }">
+        <select class="form-select" v-bind="field" :class="{ 'is-invalid': errors.length !== 0 }">
+          <option value="Select a rating">Select a rating</option>
+          <option v-for="rating in ratingArray" :key="rating" :value="rating">
+            {{ rating }}
+          </option>
+        </select>
+
+        <div class="input_alert" v-if="errors.length !== 0">
+          {{ errorMessage }}
+        </div>
+      </Field>
+    </div>
+
+    <!-- 6-IMG -->
+    <div class="mb-4">
+      <Field name="img" v-slot="{ field, errors, errorMessage }">
+        <input
+          type="text"
+          placeholder="Add the source of the img"
+          v-bind="field"
+          class="form-control"
+          :class="{ 'is-invalid': errors.length !== 0 }"
+        />
+        <div class="input_alert" v-if="errors.length !== 0">
+          {{ errorMessage }}
+        </div>
+      </Field>
+    </div>
+
+    <v-btn type="submit" variant="outlined"> Add article </v-btn>
+  </Form>
+</template>
