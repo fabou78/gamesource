8efa3a920a1574b663632cb11f89db8079e0db82
Fabou78 <fabou@fabou.com>
2025-04-15 18:37:47 +0100

    Showing the article table

--- src/components/user/dashboard/admin/ViewArticles.vue
+++ src/components/user/dashboard/admin/ViewArticles.vue
@@ -5,16 +5,57 @@
   import { useArticleStore } from '@/stores/articles'
   const articleStore = useArticleStore()
 
+  // Router
+  import { useRoute, useRouter } from 'vue-router'
+  const router = useRouter()
+  const route = useRoute()
+
   const loading = ref(false)
   const btnLoad = ref(false)
 
   /// Load more article
 
   /// Get first article
-  loading.value = true
-  articleStore.adminViewAticles(3).finally(() => {
-    loading.value = false
-  })
+  if (!articleStore.adminArticles || route.query.reload) {
+    loading.value = true
+    articleStore.adminViewAticles(3).finally(() => {
+      loading.value = false
+    })
+  }
 </script>
 
-<template>View article</template>
+<template>
+  <div class="text-center m-3" v-show="loading">
+    <v-progress-circular indeterminate color="primary" />
+  </div>
+  <div v-show="!loading">
+    <v-table theme="dark">
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Owner</th>
+          <th class="text-left">Rating</th>
+          <th class="text-left">Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="article in articleStore.adminArticles" :key="article.id">
+          <td>{{ article.game }}</td>
+          <td>{{ article.owner.firstname }} {{ article.owner.lastname }}</td>
+          <td>{{ article.rating }}</td>
+          <td>{{ article.timestamp.toDate().toDateString() }}</td>
+          <td><v-btn variant="outlined" color="red" size="small">Remove</v-btn></td>
+          <td>
+            <v-btn
+              variant="outlined"
+              color="yellow"
+              size="small"
+              @click="router.push({ name: 'edit_article', params: { id: article.id } })"
+              >Edit</v-btn
+            >
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
+</template>
